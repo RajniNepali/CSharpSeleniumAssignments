58eb8a910ad44eaeb16e8b9bab8c97b7d79ee55e
Rajni Nepali <rajni.nepali@nitorinfotech.net>
2019-05-09 18:12:16 +0530

    Commit test1

--- SeleniumTraining/NUnitTests/UnitTest1.cs
+++ SeleniumTraining/NUnitTests/UnitTest1.cs
@@ -1,9 +1,11 @@
 using NUnit.Framework;
-
+using Scripts;
 namespace Tests
 {
     public class Tests
     {
+        private const bool Obj = true;
+        Class1 c1 = new Class1();
         [SetUp]
         public void Setup()
         {
@@ -12,7 +14,11 @@ namespace Tests
         [Test]
         public void Test1()
         {
+            var ret=c1.testCond();
+            if(ret.Equals(Obj))
             Assert.Pass();
+            else
+            Assert.Fail();
         }
     }
 }
--- SeleniumTraining/Scripts/Class1.cs
+++ SeleniumTraining/Scripts/Class1.cs
@@ -4,5 +4,11 @@ namespace Scripts
 {
     public class Class1
     {
+        public string testCond()
+        {
+            Console.WriteLine("Your method is called");
+            string str=Console.ReadLine();
+            return str;
+        }
     }
 }
