10572e30eb77e1a61ea918089bf89d5513cf6462
Rajni Nepali <rajni.nepali@nitorinfotech.net>
2019-05-10 12:48:15 +0530

    Commit for Browser lauch Script

--- SeleniumTraining/NUnitTests/UnitTest1.cs
+++ SeleniumTraining/NUnitTests/UnitTest1.cs
@@ -1,9 +1,16 @@
 using NUnit.Framework;
 using Scripts;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+
+
 namespace Tests
 {
     public class Tests
     {
+       private IWebDriver driver;
         private const bool Obj = true;
         Class1 c1 = new Class1();
         [SetUp]
@@ -14,11 +21,33 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            var ret=c1.testCond();
-            if(ret.Equals(Obj))
-            Assert.Pass();
-            else
-            Assert.Fail();
+            Test1ChromeTest();
+            // var ret=c1.testCond();
+            // if(ret.Equals(Obj))
+            // Assert.Pass();
+            // else
+            // Assert.Fail();
+        }
+        public void Test1ChromeTest()
+        {
+            driver = new ChromeDriver(@"C:\CSharpSelenium\SeleniumTraining\NUnitTests\drivers");
+           // driver.Navigate().GoToUrl("https://Google.com");
+            LaunchSiteTest();
+           // driver.Close();
+          
+        }
+        public void LaunchSiteTest()
+        {
+            driver.Navigate().GoToUrl("http://shop.demoqa.com");
+            SearchInput();
+        }
+
+
+        public void SearchInput()
+        {
+            driver.FindElement(By.ClassName("noo-search")).Click();
+            driver.FindElement(By.XPath("//input[@type='search']")).SendKeys("Rajni");
+           // IWebElement element = driver.FindElement(By.XPath());
         }
     }
 }
