8b74c4e421574a033b9ba8fae6ee4881ede3a5c4
Rajni Nepali <rajninepali0694@gmail.com>
2019-05-13 19:37:45 +0530

    Assignment1-Prarag

---
+++ Assignment_ParagKulkarni/Scripts1/Assignment1.cs
@@ -0,0 +1,34 @@
+﻿
+//Convert the string to Title Case and Camel case.
+using System;
+
+namespace Scripts1
+{
+    public class Assignment1
+    {
+        string input = "csharp Corner Home", appentstr = null,finalstr=null;
+        string[] output = null;
+        public string[] toTitleCase()
+        {
+            string[] strsplit = input.Split(" ");
+            for (int i = 0; i < strsplit.Length; i++)
+            {
+                //char[] strarr=strsplit[i].ToCharArray();
+                //output[i]=strarr[0].ToString().ToUpper();
+                for (int j = 0; j < strsplit[i].Length; j++)
+                {
+                    if(j==0)
+                    {
+                    appentstr = strsplit[i][j].ToString().ToUpper();
+                    }else{
+                            appentstr += strsplit[i][j].ToString().ToLower();
+                    }
+                }
+                finalstr +=appentstr;
+
+            }
+
+            return output;
+        }
+    }
+}
--- Assignment_ParagKulkarni/Scripts1/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Scripts1
-{
-    public class Class1
-    {
-    }
-}
